894e8ff9821296bb56b66a945810a0bdc00cf4e6
Minji Kwon <minjik95@naver.com>
2019-11-20 20:38:56 +0900

    Main UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,213 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './ask_logo.png';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// logo color => rgb: 165 0 33, hex: #a50021
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0, 0, 0, 0.8)'
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+    backgroundColor: '#000000'
+  },
+  image: {
+    flexGrow: 1,
+    maxWidth: 100,
+    paddingTop: 6
+  },
+  login_btn: {
+    color: '#ffffff',
+    border: '1px solid rgba(255, 255, 255)'
+  },
+  body: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  page_explaination: {
+    width: '500px',
+    color: '#ffffff'
+  },
+  howto_btn: {
+    backgroundColor: '#a50021',
+    color: '#ffffff',
+    '&:hover': {
+      background: '#a50021'
+    }
+  },
+  howtoclosing_btn: {
+    color: '#a50021'
+  },
+  textField: {
+    '& .MuiInputBase-root': {
+      color: '#ffffff'
+    },
+    '& .MuiFormLabel-root': {
+      color: '#ffffff'
+    },
+    '& .MuiInput-underline:before': {
+      borderBottom: '1px solid rgb(255, 255, 255)'
+    },
+    '& .MuiInput-underline:after': {
+      borderBottom: '1px solid rgb(165, 0, 33)'
+    },
+    '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+      borderBottom: '1px solid rgb(165, 0, 33)'
+    }
+  },
+  signup_btn: {
+    backgroundColor: '#a50021',
+    color: '#ffffff',
+    '&:hover': {
+      background: '#a50021'
+    }
+  },
+})
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      member: '',
+      username: '',
+      email: '',
+      password: '',
+      open: false
+    }
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({member: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/join');
+    const body = await response.json();
+    return body;
+  }
+
+  handleOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  }
+
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    this.join()
+      .then(response => {
+        console.log(response);
+      })
+
+    this.setState({
+      member: '',
+      username: '',
+      email: '',
+      password: '',
+      open: false
+    })
+    
+  }
+
+  join = () => {
+    const url = '/join';
+    const formData = new FormData();
+    formData.append('username', this.state.username);
+    formData.append('email', this.state.email);
+    formData.append('password', this.state.password);
+
+    return postMessage(url, formData);
+  }
+
+  render() {
+    const { classes } = this.props;
+    return(
+      <div className={classes.root}>
+
+        <AppBar position="static">
+          <Toolbar className={classes.toolbar}>
+            <img src={logo} alt="logo" className={classes.image}/>
+            <Button variant="outlined" className={classes.login_btn} href="http://www.google.com/">
+              로그인
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.body}>
+          <div className={classes.page_explaination}>
+            <div>아스크</div>
+            <div>
+              다른 사람들 앞에서 질문하기 어려워 하는 사람들을 위한 채팅 프로그램.
+              질문 시간이 주어지면 질문하지 않지만 따로 찾아가거나 채팅 메신저로는
+              많은 질문을 하는 당신! 모르는 것이 부끄러운 것이 아니라는 것을 머리로는 알지만
+              다른 사람들 앞에서 질문하기 어려운 당신! 이 프로그램을 쓰세요! 사용방법을
+              알고 싶으시면 아래 버튼을 눌러주세요.
+            </div>
+            <div>
+              <Button variant="contained" className={classes.howto_btn} onClick={this.handleOpen}>
+                사용방법 
+              </Button>
+              <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle>{"아스크 사용방법"}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    1. 이렇게 하세요.<br/>
+                    2. 저렇게 하세요.<br/>
+                    3. 네네 그겁니다.<br/>
+                    4. 네네 바로 그겁니다.<br/>
+                    5. 이해하셨나요?<br/>
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={this.handleClose} className={classes.howtoclosing_btn}>닫기</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+          </div>
+
+          <form>
+            <TextField type="text" name="username" label="사용자 이름" value={this.state.username} onChange={this.handleValueChange} className={classes.textField}/><br/>
+            <TextField type="text" name="email" label="이메일" value={this.state.email} onChange={this.handleValueChange} className={classes.textField}/><br/>
+            <TextField type="password" name="password" label="비밀번호" value={this.state.password} onChange={this.handleValueChange} autoComplete="current-password" className={classes.textField}/><br/>
+
+            <Button variant="contained" className={classes.signup_btn} onClick={this.handleFormSubmit}>
+              회원가입
+            </Button>
+        
+          </form>
+        </div>
+    
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
